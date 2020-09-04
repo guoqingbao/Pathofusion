92daf9de6024f68a56447aa08e3a00bd3cc2faa5
Guoqing Bao <topon@outlook.com>
2020-09-04 18:45:34 +0800

    upload code for the conference paper

---
+++ models/resnet50.py
@@ -0,0 +1,92 @@
+'''ResNet in PyTorch.
+Reference:
+He K, Zhang X, Ren S, Sun J. Deep residual learning for image recognition. 
+InProceedings of the IEEE conference on computer vision and pattern recognition 2016 (pp. 770-778).
+'''
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+def conv3x3_bn(in_planes, out_planes, stride=1, kernels=[]):
+    return nn.Sequential(nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(out_planes))
+
+
+class BasicBlock(nn.Module):
+    def __init__(self,inplanes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+
+        self.conv_bn = conv3x3_bn(inplanes, planes, stride)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        if inplanes != planes:
+            self.downsample = nn.Sequential(nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False),
+                                            nn.BatchNorm2d(planes))
+        else:
+            self.downsample = lambda x: x
+        self.stride = stride
+
+    def forward(self, x):
+        residual = self.downsample(x)
+        out = self.conv_bn(x)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet50(nn.Module):
+    def __init__(self, n_size=8, num_classes=10):
+        super(ResNet50, self).__init__()
+        self.inplane = 16
+        self.conv1 = nn.Conv2d(3, self.inplane, kernel_size=3, stride=2, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.inplane)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(16, blocks=n_size, stride=1)
+        self.layer2 = self._make_layer(32, blocks=n_size, stride=2)
+        self.layer3 = self._make_layer(64, blocks=n_size, stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(64, num_classes)
+
+        self.initialize()
+
+    def initialize(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, planes, blocks, stride):
+
+        strides = [stride] + [1] * (blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(BasicBlock(self.inplane, planes, stride))
+            self.inplane = planes
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
